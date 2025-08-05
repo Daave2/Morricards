dcedb68b1eb28dd8774e70be5dc39d8515a7d522
Niki Cooke <DAVE3K@gmail.com>
2025-08-05 17:44:03 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/availability/page.tsx
+++ src/app/availability/page.tsx
@@ -26,6 +26,7 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
+  AlertDialogTrigger,
 } from "@/components/ui/alert-dialog"
 import {
   Dialog,
@@ -679,9 +680,3 @@ export default function AvailabilityPage() {
     </div>
   );
 }
-
-    
-
-    
-
-    
