fd46c0e8259feb411d31e40f9c393211853b829d
Niki Cooke <DAVE3K@gmail.com>
2025-08-11 12:55:00 +0000

    ChunkLoadError     at __webpack_require__.f.j (https://6000-firebase-stu

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,43 @@
-/** @type {import('next').NextConfig} */
+
 import withPWA from 'next-pwa';
+
+const isDevelopment = process.env.NODE_ENV === 'development';
 
 const pwaConfig = withPWA({
   dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
+  disable: isDevelopment,
+  // Add an empty `swSrc` in dev to avoid "InjectManifest" errors if it tries to run
+  swSrc: isDevelopment ? '/dev/null' : undefined, 
 });
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 's3-eu-west-1.amazonaws.com',
+        port: '',
+        pathname: '/**',
       },
+       {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      }
     ],
   },
   experimental: {
-    // NOTE: appDir is enabled by default in Next.js 13.4+
+    allowedDevOrigins: ["https://6000-firebase-studio-1754152994600.cluster-cbeiita7rbe7iuwhvjs5zww2i4.cloudworkstations.dev"],
   },
-  // This is the correct location for allowedDevOrigins
-  allowedDevOrigins: [
-    'https://6000-firebase-studio-1754152994600.cluster-cbeiita7rbe7iuwhvjs5zww2i4.cloudworkstations.dev',
-  ],
+  webpack: (config, { isServer }) => {
+    // Disable PWA workbox generation in development
+    if (isDevelopment && !isServer) {
+      config.plugins = config.plugins.filter(
+        (plugin) => plugin.constructor.name !== 'InjectManifest'
+      );
+    }
+    return config;
+  },
 };
 
 export default pwaConfig(nextConfig);
