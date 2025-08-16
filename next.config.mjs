93f76d5146e2773c59aaaf85ea4bc1c4879e50d9
Niki Cooke <DAVE3K@gmail.com>
2025-08-16 19:25:12 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,29 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 's3-eu-west-1.amazonaws.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-      },
-    ],
-  },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals.push({
-        handlebars: 'commonjs handlebars',
-      });
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 's3-eu-west-1.amazonaws.com',
+                port: '',
+                pathname: '/mprod.xxwmm.retail.brandbank.zzzzzzzzzz/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'placehold.co',
+            }
+        ],
+    },
+    webpack: (config, { isServer }) => {
+        if (isServer) {
+            config.externals.push('handlebars');
+        }
+        return config;
+    },
+    experimental: {
+        allowedDevOrigins: ["https://6000-firebase-studio-1754152994600.cluster-cbeiita7rbe7iuwhvjs5zww2i4.cloudworkstations.dev"]
     }
-    return config;
-  },
 };
 
 export default nextConfig;
