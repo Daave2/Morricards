6ea121f36aca70b0e4ad1c6ae89499e3e0555865
Niki Cooke <DAVE3K@gmail.com>
2025-08-17 20:50:22 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- app/assistant/page.tsx
+++ app/assistant/page.tsx
@@ -1,7 +1,7 @@
 
 'use client';
 
-import { useCallback, useEffect, useRef, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import * as z from 'zod';
