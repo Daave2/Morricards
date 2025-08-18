906583876b938bb2369aa05c3ebcf50b299b8bb8
Niki Cooke <DAVE3K@gmail.com>
2025-08-18 17:28:00 +0000

    do we have a favicon set?

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,10 @@ export const metadata: Metadata = {
   title: 'MorriCards',
   description: 'Your friendly shopping assistant.',
   manifest: '/manifest.webmanifest',
+  icons: {
+    icon: '/icons/icon-192x192.png',
+    apple: '/icons/icon-192x192.png',
+  },
 };
 
 export const viewport: Viewport = {
