a37c923384ba5fab8b3ed7628608123590208817
Niki Cooke <DAVE3K@gmail.com>
2025-08-19 12:35:40 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.js
+++ next.config.js
@@ -22,6 +22,10 @@ const nextConfig = {
             },
             {
                 protocol: 'https',
+                hostname: 'images.morrisons.com',
+            },
+            {
+                protocol: 'https',
                 hostname: 's3-eu-west-1.amazonaws.com',
             },
         ],
