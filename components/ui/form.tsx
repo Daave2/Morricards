dedd52f7def08586c1bd43004e492977e164734a
Niki Cooke <DAVE3K@gmail.com>
2025-08-20 19:08:15 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- components/ui/form.tsx
+++ components/ui/form.tsx
@@ -45,13 +45,25 @@ const FormField = <
 const useFormField = () => {
   const fieldContext = React.useContext(FormFieldContext)
   const itemContext = React.useContext(FormItemContext)
-  const { getFieldState, formState } = useFormContext()
-
-  const fieldState = getFieldState(fieldContext.name, formState)
+  const form = useFormContext()
 
   if (!fieldContext) {
     throw new Error("useFormField should be used within <FormField>")
   }
+  
+  if (!form) {
+    return {
+      id: itemContext.id,
+      name: fieldContext.name,
+      formItemId: `${itemContext.id}-form-item`,
+      formDescriptionId: `${itemContext.id}-form-item-description`,
+      formMessageId: `${itemContext.id}-form-item-message`,
+      error: null
+    }
+  }
+
+  const { getFieldState, formState } = form;
+  const fieldState = getFieldState(fieldContext.name, formState)
 
   const { id } = itemContext
 
