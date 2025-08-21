731cc8c6fb299a9f3f6511538944d323d9a7f4f4
Niki Cooke <DAVE3K@gmail.com>
2025-08-21 17:30:39 +0000

    still low?

--- components/ui/dialog.tsx
+++ components/ui/dialog.tsx
@@ -23,6 +23,7 @@ const DialogOverlay = React.forwardRef<
     ref={ref}
     className={cn(
       "fixed inset-0 z-50 bg-black/80 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
+      "theme-glass:bg-black/10 theme-glass:backdrop-blur-sm",
       className
     )}
     {...props}
