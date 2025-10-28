66ce3d3ee5bcb58ed0db3ab55f1916a799f9fbb9
Niki Cooke <DAVE3K@gmail.com>
2025-10-28 20:24:03 +0000

    its blank

--- app/storemapper/page.tsx
+++ app/storemapper/page.tsx
@@ -5,7 +5,7 @@ import { Card, CardContent } from "@/components/ui/card";
 
 export default function StoreMapperPage() {
   return (
-    <div className="h-[calc(100vh-56px)] md:h-[calc(100vh-88px)] w-full p-0 m-0">
+    <div className="h-[calc(100vh-120px)] md:h-[calc(100vh-56px)] w-full p-0 m-0">
        <iframe
           src="https://storemapper-ai-584939250419.us-west1.run.app"
           className="w-full h-full border-0"
