e361720b5a4c1ae1ca3e1ef2bc1f775f226941b1
Niki Cooke <DAVE3K@gmail.com>
2025-11-25 13:53:28 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "morricards",
+  "appId": "1:19314257763:web:882f5ab308ec91b40ded56",
+  "apiKey": "AIzaSyBhtnJ4meofnKy851E5D6tHN_kmRD9gol0",
+  "authDomain": "morricards.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "19314257763"
+};
