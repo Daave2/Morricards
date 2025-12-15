a1f13dfc6eff45633d3eed14e5acf6fd90f7621a
Niki Cooke <DAVE3K@gmail.com>
2025-12-15 23:16:38 +0000

    and the amazon page?

---
+++ workspace/app/assistant/page.tsx
@@ -0,0 +1,6 @@
+import AssistantPageClient from "./AssistantPageClient";
+
+// This is now a static Server Component that renders the client component.
+export default function AssistantPage() {
+  return <AssistantPageClient />;
+}
